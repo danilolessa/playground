01b62baf398675495628910fe4126621da8d8140
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2016-07-09 22:35:40 -0300

    working on pythonroot

--- pythonroot/func.py
+++ pythonroot/func.py
@@ -3,6 +3,52 @@ import scipy as sp
 import scipy.odr as odr
 import matplotlib.pyplot as plt
 
+class InstanceOptions:
+    name = ""
+    title = ""
+    def __init__(self):
+        return None
+    
+class AxisOptions:
+    axis_title = ""
+    axis_interval = None
+    axis_log = False
+    axis_grad = False
+    
+    axis_title_style = None
+    axis_mark_style = None
+
+class PlotOptions:
+
+    title = None
+    windowSize = None
+    fontType = None
+    backgroundColor = None
+    frontgroundColor = None
+    pointStyle = None
+    errorbarStyle = None
+    x_axis_options = None ###
+    y_axis_options = None ###
+    
+    def __init__(self):
+        return None
+
+class FitOptions:
+
+    function = None
+    parameters = None
+    fit = False
+    functionStyle = None
+    
+    #Fitting params
+    fit_interval = None    
+    fix_parameters = None
+    fit_mode = None
+    residualPlot_type = None
+    residual_axis_options = None ###
+
+    def __init__(self):
+        return None
 
 def odr_fit(func, x, y, sx, sy, beta0=None):
     model = odr.Model(func)
@@ -11,34 +57,26 @@ def odr_fit(func, x, y, sx, sy, beta0=None):
     return odr_inst.run()
 
 
-def plot(x, y, sx=None, sy=None, output=True, fit=None, fit_func=None,
-     fit_params=None, residuals=False, xlim=None, ylim=None):
+
+
+def plot(x, y, sx=None, sy=None, output=True, fitOptions=None, plotOptions=None):
     #initial
     if sx is None:
         sx = np.zeros(len(x))
     if sy is None:
         sy = np.zeros(len(y))
 
-    #limits
-    if xlim is None:
-        (xmin, xmax) = (np.min(x), np.max(x))
-    else:
-        (xmin, xmax) - (xlim[0], xlim[1])
-    if ylim is None:
-        (ymin, ymax) = (np.min(y), np.max(y))
-    else:
-        (ymin, ymax) - (ylim[0], ylim[1])
-
     #data ploting
-    plt.errorbar(x, y, xerr=sx, yerr=sy, fmt='.')
+    ax = plt.figure()
+    ax.errorbar(x, y, xerr=sx, yerr=sy, fmt='.')
 
     #fitting
-    if fit is True and fit_params is not None:
-        fit_output = odr_fit(fit_func, x, y, sx, sy, fit_params)
-        x_fit = np.linspace(xmin, xmax, 1e5)
-        y_fit = fit_func(fit_output.beta, x_fit)
-        plt.plot(x_fit, y_fit, 'k-')
 
     if output is True:
         plt.show()
+            
+    
+    return None
+    
+
 
